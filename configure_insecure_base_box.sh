2da8d4a49591940463b4961197562d7efdddbf82
Dolph Mathews <dolph.mathews@gmail.com>
2015-01-09 11:26:06 -0600

    installing well known keys

--- configure_insecure_base_box.sh
+++ configure_insecure_base_box.sh
@@ -7,7 +7,18 @@ fi
 command -v ansible-playbook >/dev/null 2>&1 || { echo "Switch to a Python virtualenv with ansible installed (pip install ansible)."; exit 1; }
 
 # this makes using ansible trivial, and we can cleanup later
-ssh-copy-id root@$1
+echo "Installing authorized key..."
+ssh-copy-id root@$1 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
+
+# stomp on the remote system's existing identity
+echo "Uploading well-known private key..."
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no roles/bootstrap/files/vagrant root@$1:~/ssh_host_rsa_key
+echo "Uploading well-known public key..."
+scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no roles/bootstrap/files/vagrant.pub root@$1:~/ssh_host_rsa_key.pub
+echo "Deploying well-known SSH identity..."
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@$1 "chmod 0600 ~/ssh_host_rsa_key && chmod 0644 ssh_host_rsa_key.pub && mv ~/ssh_host_rsa_key* /etc/ssh"
+echo "Please verify the server's new identity..."
+ssh root@$1 "echo Success."
 
 ansible-playbook -i "$1," --user="root" bootstrap.yaml
 
